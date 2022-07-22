d9096d4f615267e99a82fd7fb3eb0f9b30dd7ac6
Seiji Ito <seijiito1102@outlook.com>
2022-07-22 17:23:40 +0800

    add max limit per user on public sale
    - add the max limit on public sale
    - add a function for updating max supply

--- nft_whitelist_mint/tasks/params.ts
+++ nft_whitelist_mint/tasks/params.ts
@@ -1,23 +1,24 @@
 import { ethers } from "ethers";
 
-export const NFT_NAME = "3000 KINGS";
-export const NFT_SYMBOL = "3KN";
+export const NFT_NAME = "Onigiri Metaverse";
+export const NFT_SYMBOL = "ONI";
 export const MAX_SUPPLY = 100;
-export const URI_PREFIX = "https://ipfs.io/ipfs/QmbpTCy1KoUz2E2pV5zrMafZzgfwFS9mRWRFzyRYQ9vuNo/";
+export const URI_PREFIX = "https://ipfs.io/ipfs/QmQbFiHNDddrRD4Tz1baMWoy1ej4GjurBSw282oE2eqXKo/";
 
 function getTimestampFromDate(date: Date) {
   return Math.floor(date.getTime() / 1000);
 }
 
-// Whitelist mint schedule 1
+// Whitelist mint schedule
 export const MERKLE_ROOT1 = "0x93efe2dd21e75ef7fd7aa0258254724645b7e134e8587aa37ab72e855ef4ef23";
-export const START_TIME1 = getTimestampFromDate(new Date("2022-07-19T00:00:00Z"));
-export const END_TIME1 = getTimestampFromDate(new Date("2022-07-20T12:00:00Z"));
-export const MINT_PRICE1 = ethers.utils.parseEther("0.01");
-export const MERKLE_TOTAL = 30;
+export const START_TIME1 = getTimestampFromDate(new Date("2022-07-22T10:00:00Z"));
+export const END_TIME1 = getTimestampFromDate(new Date("2022-07-25T12:00:00Z"));
+export const MINT_PRICE1 = ethers.utils.parseEther("0.01"); // 0.01ETH
+export const MERKLE_TOTAL = 30; // Merkle total amount
 
 // Public mint schedule
-export const START_TIME2 = getTimestampFromDate(new Date("2022-07-14T12:00:00Z"));
-export const END_TIME2 = getTimestampFromDate(new Date("2022-07-20T12:00:00Z"));
-export const MINT_PRICE2 = ethers.utils.parseEther("0.1");
-export const MINT_AMOUNT2 = 10;
+export const START_TIME2 = getTimestampFromDate(new Date("2022-07-23T00:00:00Z"));
+export const END_TIME2 = getTimestampFromDate(new Date("2022-07-25T12:00:00Z"));
+export const MINT_PRICE2 = ethers.utils.parseEther("0.01"); // 0.01ETH
+export const MINT_AMOUNT2 = 20; // Public mint total amount
+export const MAX_PER_USER = 2; // 0: Infinite, > 0: Check max
