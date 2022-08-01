ca4cba667e77fb9507a9d9b3cd9551b84e62c15e
Seiji Ito <seijiito1102@outlook.com>
2022-08-02 02:40:54 +0800

    update params

--- nft_whitelist_mint/tasks/params.ts
+++ nft_whitelist_mint/tasks/params.ts
@@ -10,15 +10,15 @@ function getTimestampFromDate(date: Date) {
 }
 
 // Whitelist mint schedule
-export const MERKLE_ROOT1 = "0x88be36133ad52c48aa12241bb3395668b40bc6d4f5c6b897b0006cdd068dceee";
+export const MERKLE_ROOT1 = "0x0718aaad465d4061f109d77b00c93b9b776f44b5f61cbeb11d779ba5ee7ed8c6";
 export const START_TIME1 = getTimestampFromDate(new Date("2022-08-01T05:00:00Z"));
 export const END_TIME1 = getTimestampFromDate(new Date("2022-08-02T00:00:00Z"));
-export const MINT_PRICE1 = ethers.utils.parseEther("0.01"); // 0.01ETH
-export const MERKLE_TOTAL = 30; // Merkle total amount
+export const MINT_PRICE1 = ethers.utils.parseEther("0.025"); // 0.01ETH
+export const MERKLE_TOTAL = 0; // 0: Infinite, > 0: Merkle total amount
 
 // Public mint schedule
 export const START_TIME2 = getTimestampFromDate(new Date("2022-08-01T16:40:00Z"));
 export const END_TIME2 = getTimestampFromDate(new Date("2022-08-02T00:00:00Z"));
-export const MINT_PRICE2 = ethers.utils.parseEther("0.01"); // 0.01ETH
-export const MINT_AMOUNT2 = 20; // Public mint total amount
-export const MAX_PER_USER = 0; // 0: Infinite, > 0: Check max
+export const MINT_PRICE2 = ethers.utils.parseEther("0.03"); // 0.01ETH
+export const MINT_AMOUNT2 = 0; // 0: Infinite, > 0: Public mint total amount
+export const MAX_PER_USER = 0; // 0: Infinite, > 0: Max amount
