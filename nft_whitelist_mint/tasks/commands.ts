b4c8bdbb345bca073fe71dafc64fbf73d020dee4
Seiji Ito <seijiito1102@outlook.com>
2022-08-03 12:30:15 +0800

    update params

--- nft_whitelist_mint/tasks/commands.ts
+++ nft_whitelist_mint/tasks/commands.ts
@@ -3,17 +3,22 @@ import { TaskArguments } from "hardhat/types";
 import {
   END_TIME1,
   END_TIME2,
-  MAX_PER_USER,
+  END_TIME3,
+  MAX_PER_USER3,
   MERKLE_ROOT1,
-  MERKLE_TOTAL,
+  MERKLE_ROOT2,
+  MINT_AMOUNT1,
   MINT_AMOUNT2,
+  MINT_AMOUNT3,
   MINT_PRICE1,
   MINT_PRICE2,
+  MINT_PRICE3,
   START_TIME1,
   START_TIME2,
+  START_TIME3,
 } from "./params";
 
-task("set:WhitelistSale", "Set whitelist sale to CGCWhitelistERC721A nft contract")
+task("set:WhitelistSale1", "Set whitelist sale to CGCWhitelistERC721A nft contract")
   .addParam("address", "The deployed smart contract address")
   .setAction(async function (taskArguments: TaskArguments, hre) {
     const cgcWhitelistERC721A = await hre.ethers.getContractAt("CGCWhitelistERC721A", taskArguments.address);
@@ -24,25 +29,43 @@ task("set:WhitelistSale", "Set whitelist sale to CGCWhitelistERC721A nft contrac
       START_TIME1,
       END_TIME1,
       MINT_PRICE1,
-      MERKLE_TOTAL
+      MINT_AMOUNT1
     );
     await tx.wait();
 
     console.log("New whitelist sale was set by transaction:", tx.hash);
   });
 
-task("set:PublicSale", "Set whitelist sale to CGCWhitelistERC721A contract")
+task("set:WhitelistSale2", "Set whitelist sale to CGCWhitelistERC721A nft contract")
+  .addParam("address", "The deployed smart contract address")
+  .setAction(async function (taskArguments: TaskArguments, hre) {
+    const cgcWhitelistERC721A = await hre.ethers.getContractAt("CGCWhitelistERC721A", taskArguments.address);
+
+    // Deploy Contract
+    const tx = await cgcWhitelistERC721A.setupWhitelistSale(
+      MERKLE_ROOT2,
+      START_TIME2,
+      END_TIME2,
+      MINT_PRICE2,
+      MINT_AMOUNT2
+    );
+    await tx.wait();
+
+    console.log("New whitelist sale was set by transaction:", tx.hash);
+  });
+
+task("set:PublicSale3", "Set whitelist sale to CGCWhitelistERC721A contract")
   .addParam("address", "The deployed smart contract address")
   .setAction(async function (taskArguments: TaskArguments, hre) {
     const cgcWhitelistERC721A = await hre.ethers.getContractAt("CGCWhitelistERC721A", taskArguments.address);
 
     // Deploy Contract
     const tx = await cgcWhitelistERC721A.setupPublicSale(
-      START_TIME2,
-      END_TIME2,
-      MINT_PRICE2,
-      MINT_AMOUNT2,
-      MAX_PER_USER
+      START_TIME3,
+      END_TIME3,
+      MINT_PRICE3,
+      MINT_AMOUNT3,
+      MAX_PER_USER3
     );
     await tx.wait();
 
--- nft_whitelist_mint/tasks/params.ts
+++ nft_whitelist_mint/tasks/params.ts
@@ -1,24 +1,31 @@
 import { ethers } from "ethers";
 
-export const NFT_NAME = "Onigiri Metaverse NFT";
-export const NFT_SYMBOL = "ONI";
+export const NFT_NAME = "Onigiri Metaverse";
+export const NFT_SYMBOL = "ONIGIRI";
 export const MAX_SUPPLY = 3000;
-export const URI_PREFIX = "https://dev-api.onigirimetaverse.com/nft/";
+export const URI_PREFIX = "https://api.onigirimetaverse.com/nft/";
 
 function getTimestampFromDate(date: Date) {
   return Math.floor(date.getTime() / 1000);
 }
 
-// Whitelist mint schedule
+// Whitelist mint schedule (Phase1)
 export const MERKLE_ROOT1 = "0x0718aaad465d4061f109d77b00c93b9b776f44b5f61cbeb11d779ba5ee7ed8c6";
-export const START_TIME1 = getTimestampFromDate(new Date("2022-08-01T05:00:00Z"));
-export const END_TIME1 = getTimestampFromDate(new Date("2022-08-02T00:00:00Z"));
-export const MINT_PRICE1 = ethers.utils.parseEther("0.025"); // 0.01ETH
-export const MERKLE_TOTAL = 0; // 0: Infinite, > 0: Merkle total amount
+export const START_TIME1 = getTimestampFromDate(new Date("2022-08-03T14:30:00Z"));
+export const END_TIME1 = getTimestampFromDate(new Date("2022-08-03T14:55:00Z"));
+export const MINT_PRICE1 = ethers.utils.parseEther("0.02"); // 0.02ETH
+export const MINT_AMOUNT1 = 150; // 0: Infinite, > 0: Merkle total amount
 
-// Public mint schedule
-export const START_TIME2 = getTimestampFromDate(new Date("2022-08-02T06:50:00Z"));
-export const END_TIME2 = getTimestampFromDate(new Date("2022-08-06T00:00:00Z"));
-export const MINT_PRICE2 = ethers.utils.parseEther("0.01"); // 0.01ETH
-export const MINT_AMOUNT2 = 0; // 0: Infinite, > 0: Public mint total amount
-export const MAX_PER_USER = 0; // 0: Infinite, > 0: Max amount
+// Whitelist mint schedule (Phase2)
+export const MERKLE_ROOT2 = "0x0718aaad465d4061f109d77b00c93b9b776f44b5f61cbeb11d779ba5ee7ed8c6";
+export const START_TIME2 = getTimestampFromDate(new Date("2022-08-03T15:00:00Z"));
+export const END_TIME2 = getTimestampFromDate(new Date("2022-08-03T15:25:00Z"));
+export const MINT_PRICE2 = ethers.utils.parseEther("0.025"); // 0.025ETH
+export const MINT_AMOUNT2 = 300; // 0: Infinite, > 0: Merkle total amount
+
+// Public mint schedule (Phase3)
+export const START_TIME3 = getTimestampFromDate(new Date("2022-08-03T15:30:00Z"));
+export const END_TIME3 = getTimestampFromDate(new Date("2022-08-04T15:30:00Z"));
+export const MINT_PRICE3 = ethers.utils.parseEther("0.03"); // 0.03ETH
+export const MINT_AMOUNT3 = 2350; // 0: Infinite, > 0: Public mint total amount
+export const MAX_PER_USER3 = 0; // 0: Infinite, > 0: Max amount
